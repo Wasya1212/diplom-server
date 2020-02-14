a0acdb816a37be0e359ecac90fcabf50e3cc8787
wasya1212 <leonardo.1212@yandex.ua>
2020-02-14 20:58:33 +0200

    delete mongoose connect

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -1,14 +1,14 @@
 const Mongoose = require('mongoose');
 
 module.exports.connect = () => {
-  Mongoose
-    .connect(
-      'mongodb+srv://wasya1212:wasya1212@cluster0-v4ayb.mongodb.net/test?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    )
-    .then(() => console.log("MongoDb connected..."))
-    .catch(err => console.error(err));
+  // Mongoose
+  //   .connect(
+  //     'mongodb+srv://wasya1212:wasya1212@cluster0-v4ayb.mongodb.net/test?retryWrites=true&w=majority',
+  //     {
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true
+  //     }
+  //   )
+  //   .then(() => console.log("MongoDb connected..."))
+  //   .catch(err => console.error(err));
 };
