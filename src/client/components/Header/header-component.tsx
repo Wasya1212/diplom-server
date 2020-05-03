c797eff97fd255f0098a67207d12294e019036d2
Wasya1212 <leonardo.1212@yandex.ua>
2020-05-04 01:48:02 +0300

    added simple header style

--- src/client/components/Header/header-component.tsx
+++ src/client/components/Header/header-component.tsx
@@ -13,11 +13,24 @@ interface HeaderProps {
 
 class Header extends Component<any, {}> {
   state = {
-    addProjectModalIsOpen: false
+    addProjectModalIsOpen: false,
+    navigationIsOpen: false
   }
 
   constructor(props) {
     super(props);
+  }
+
+  showNavigation = () => {
+    this.setState({ navigationIsOpen: true });
+  }
+
+  hideNavigation = () => {
+    this.setState({ navigationIsOpen: false });
+  }
+
+  toggleNavigation = () => {
+    this.setState({ navigationIsOpen: !this.state.navigationIsOpen });
   }
 
   onProjectCreate = (newProject) => {
@@ -39,30 +52,36 @@ class Header extends Component<any, {}> {
   render() {
     if (this.props.store.user && this.props.store.user != {}) {
       return (
-        <header>
-          <nav>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/map">Map</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/sign-up">Sign up</Link></li>
-            <li><a href="logout">logout</a></li>
+        <header className="header">
+          <section onClick={this.toggleNavigation} className="header__profile">
+            <span className="header__profile__username">{this.props.store.user.name.fName}</span>
+            <span className="header__profile__post">{this.props.store.user.workInfo.post}</span>
+            <div className="header__profile__photo"><img src={this.props.store.user.photo} alt={this.props.store.user.name.fName} /></div>
+          </section>
+          <nav className={`header__navigation${this.state.navigationIsOpen ? '' : ' hidden'}`}>
+            <li className="header__navigation__item"><Link to="/">Profile</Link></li>
+            <li className="header__navigation__item"><Link to="/">Control panel</Link></li>
+            <li className="header__navigation__item"><Link to="/settings">Settings</Link></li>
+            <li className="header__navigation__item"><a href="logout">logout</a></li>
           </nav>
-          <menu>
-            {
-              ...this.props.store.user.projects.map((project, index) => (
-                <li key={`project-menu-item-${index}`}>{project.name}</li>
-              ))
-            }
+          <menu className="header__projects-list">
+            <div className="wrapper">
+              {
+                ...this.props.store.user.projects.map((project, index) => (
+                  <li className="header__projects-list__item" key={`project-menu-item-${index}`}>{project.name}</li>
+                ))
+              }
+            </div>
           </menu>
           {
             (!(this.props.store.user.workInfo.additionalInfo || {}).worker)
-              ? <button onClick={this.showAddProjectModal}>create new project</button>
+              ? <li className="header__project__add-btn"><button onClick={this.showAddProjectModal}>create new project</button></li>
               : null
           }
-          <div>
-            username: {this.props.store.user ? this.props.store.user.name.fName : ''}
-            userId: {this.props.store.user ? this.props.store.user._id : ''}
-          </div>
+          <form className="header__search-form">
+            <input name="searchData" placeholder="worker, product, route..." />
+          </form>
+          <div className="header__logo">Diplom</div>
           <Modal onClose={this.hideAddProjectModal} isOpen={this.state.addProjectModalIsOpen}>
             <CreateProjectForm onCreateProject={this.onProjectCreate} />
           </Modal>
