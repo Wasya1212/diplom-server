d6ec7d5c361954e269e7472f0fa9a78d6925f252
Wasya1212 <leonardo.1212@yandex.ua>
2020-05-04 03:09:01 +0300

    added simple modal style

--- src/client/components/Modal/modal-component.tsx
+++ src/client/components/Modal/modal-component.tsx
@@ -10,10 +10,10 @@ export class Modal extends Component<ModalProps, any> {
     if (this.props.isOpen == false) {
       return false;
     }
-    
+
     return (
       <div className={"modal" + (this.props.isOpen ? "" : " modal-hidden hidden")}>
-        <button onClick={this.props.onClose}>Close</button>
+        <button className="modal__close-btn" onClick={this.props.onClose}>Close</button>
         <section className="modal__content">{this.props.children}</section>
       </div>
     );
